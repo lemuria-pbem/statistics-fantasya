6286d582168f85d970b4ef3ff94254d1a0916929
lemuria-pbem <lemuria@online.de>
2022-04-06 19:49:47 +0200

    Implement market statistics.

--- src/Officer/Economist.php
+++ src/Officer/Economist.php
@@ -3,10 +3,14 @@ declare(strict_types = 1);
 namespace Lemuria\Statistics\Fantasya\Officer;
 
 use Lemuria\Engine\Fantasya\Statistics\Subject;
+use Lemuria\Lemuria;
 use Lemuria\Model\Fantasya\Factory\BuilderTrait;
+use Lemuria\Model\Fantasya\Luxuries;
+use Lemuria\Model\Fantasya\Statistics\Data\Market;
 use Lemuria\Statistics\Data\Number;
 use Lemuria\Statistics\Fantasya\Exception\UnsupportedSubjectException;
 use Lemuria\Statistics\Metrics;
+use Lemuria\Statistics\Record;
 
 class Economist extends AbstractOfficer
 {
@@ -28,10 +32,41 @@ class Economist extends AbstractOfficer
 				$this->storeNumber($message, $amount);
 				break;
 			case Subject::Market->name :
-				//TODO
+				$luxuries = $this->region($message)->Luxuries();
+				if ($luxuries) {
+					$statistics = Lemuria::Statistics();
+					$record     = $statistics->request(Record::from($message));
+					$market     = $this->createMarketData($record, $luxuries);
+					$statistics->store($record->setData($market));
+				}
 				break;
 			default :
 				throw new UnsupportedSubjectException($this, $message);
 		}
+	}
+
+	protected function createMarketData(Record $record, Luxuries $luxuries): Market {
+		$offer     = $luxuries->Offer();
+		$luxury    = $offer->Commodity();
+		$price     = $offer->Price();
+
+		$newMarket = new Market();
+		$market    = $record->Data();
+		if ($market instanceof Market) {
+			$newMarket[$luxury] = new Number($price, $price - $market[$luxury]->value);
+			foreach ($luxuries as $offer) {
+				$luxury             = $offer->Commodity();
+				$price              = $offer->Price();
+				$newMarket[$luxury] = new Number($price, $price - $market[$luxury]->value);
+			}
+		} else {
+			$newMarket[$luxury] = new Number($price, $price);
+			foreach ($luxuries as $offer) {
+				$luxury             = $offer->Commodity();
+				$price              = $offer->Price();
+				$newMarket[$luxury] = new Number($price, $price);
+			}
+		}
+		return $newMarket;
 	}
 }
