01faf1878560d8490636d5c5745656350286ded1
lemuria-pbem <lemuria@online.de>
2022-04-14 09:23:14 +0200

    Add party education statistics.

--- src/Officer/SchoolInspector.php
+++ src/Officer/SchoolInspector.php
@@ -6,8 +6,11 @@ use JetBrains\PhpStorm\Pure;
 
 use function Lemuria\getClass;
 use Lemuria\Engine\Fantasya\Statistics\Subject;
+use Lemuria\Model\Fantasya\Ability;
 use Lemuria\Model\Fantasya\Factory\BuilderTrait;
 use Lemuria\Model\Fantasya\Knowledge;
+use Lemuria\Model\Fantasya\Party;
+use Lemuria\Model\Fantasya\Unit;
 use Lemuria\Statistics\Fantasya\Exception\UnsupportedSubjectException;
 use Lemuria\Statistics\Metrics;
 
@@ -17,11 +20,17 @@ class SchoolInspector extends AbstractOfficer
 
 	public function __construct() {
 		parent::__construct();
+		$this->subjects[] = Subject::Education->name;
 		$this->subjects[] = Subject::Talents->name;
 	}
 
 	public function process(Metrics $message): void {
 		switch ($message->Subject()) {
+			case Subject::Education->name :
+				$party           = $this->party($message);
+				$totalExperience = $this->getTotalExperience($party);
+				$this->storeNumber($message, $totalExperience);
+				break;
 			case Subject::Talents->name :
 				$unit    = $this->unit($message);
 				$talents = $this->getTalents($unit->Knowledge());
@@ -32,11 +41,21 @@ class SchoolInspector extends AbstractOfficer
 		}
 	}
 
-	#[Pure] protected function getTalents(Knowledge $knowledge): array {
+	#[Pure] protected function getTotalExperience(Party $party): int {
+		$totalExperience = 0;
+		foreach ($party->People() as $unit /* @var Unit $unit */) {
+			foreach ($unit->Knowledge() as $ability) {
+				$totalExperience += $ability->Count();
+			}
+		}
+		return $totalExperience;
+	}
+
+	protected function getTalents(Knowledge $knowledge): array {
 		$talents = [];
-		foreach ($knowledge as $ability) {
+		foreach ($knowledge as $ability /* @var Ability $ability */) {
 			$class           = getClass($ability->getObject());
-			$talents[$class] = $ability->Count();
+			$talents[$class] = $ability->Level();
 		}
 		return $talents;
 	}
